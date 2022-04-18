96b50af4e0c472c61b2729dc075aaf7e8db6c6df
Samuel Tamayo <tamayogaviriasamuel@protonmail.com>
2022-04-18 17:29:37 -0500

    modified strategy.py

--- backtesting strategies/Backtrader/MA_RSI_15m/strategy.py
+++ backtesting strategies/Backtrader/MA_RSI_15m/strategy.py
@@ -1,5 +1,21 @@
 import backtrader as bt
-import backtrader.indicators as btind
 
 class MaRsiStrat(bt.Strategy):
-    pass
+    
+    params = (
+        ('emaperiod', 200),
+        ('rsiperiod', 2)      
+              )
+    
+    def __init__(self):
+        self.dataclose = self.datas[0].Close
+        self.ma = bt.talib.EMA(self.dataclose, timeperiod = self.p.emaperiod)
+        self.rsi = bt.talib.RSI(self.dataclose, timeperiod = self.p.rsiperiod)
+        
+    def next(self):
+        
+        if self.dataclose >= self.ema and self.rsi <= 10:
+            self.buy()
+
+        if self.dataclose <= self.ema and self.rsi >= 90:
+            self.sell()
