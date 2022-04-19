e442e5fb8693994d3388af5733e8374ff72945cc
Samuel Tamayo <tamayogaviriasamuel@protonmail.com>
2022-04-19 17:05:40 -0500

    testing the strategies

--- backtesting strategies/Backtrader/MA_RSI_15m/strategy.py
+++ backtesting strategies/Backtrader/MA_RSI_15m/strategy.py
@@ -1,4 +1,5 @@
 import backtrader as bt
+import datetime
 
 class MaRsiStrat(bt.Strategy):
     
@@ -7,6 +8,10 @@ class MaRsiStrat(bt.Strategy):
         ('rsiperiod', 2)      
               )
     
+    def log(self):
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+    
     def __init__(self):
         self.dataclose = self.datas[0].Close
         self.ma = bt.talib.EMA(self.dataclose, timeperiod = self.p.emaperiod)
@@ -14,8 +19,12 @@ class MaRsiStrat(bt.Strategy):
         
     def next(self):
         
+        self.log('Close, %.2f' % self.dataclose[0])
+        
         if self.dataclose >= self.ema and self.rsi <= 10:
+            self.log('BUY CREATED, %.2f' % self.dataclose[0])
             self.buy()
 
         if self.dataclose <= self.ema and self.rsi >= 90:
+            self.log('SELL CREATED, %.2f' % self.dataclose[0])
             self.sell()
